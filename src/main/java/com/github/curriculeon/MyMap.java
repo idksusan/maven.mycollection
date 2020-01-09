6bd04ac0db22d033246e707c9ca43f88c432cdb1
Susan <sttabassum93@gmail.com>
2020-01-09 15:47:50 -0500

    Started MyMap

--- src/main/java/com/github/curriculeon/MyMap.java
+++ src/main/java/com/github/curriculeon/MyMap.java
@@ -1,11 +1,42 @@
 package com.github.curriculeon;
 
 import java.util.List;
+import java.util.Set;
 
-public class MyMap<KeyType, ValueType> {
+public class MyMap<KeyType, ValueType> implements MyMapInterface{
     public MyMap() {
     }
 
     public MyMap(List<KeyValue<KeyType, ValueType>> entries) {
+    }
+
+    @Override
+    public void put(Object key, Object value) {
+        
+    }
+
+    @Override
+    public Object get(Object key) {
+        return null;
+    }
+
+    @Override
+    public Set getKeySet() {
+        return null;
+    }
+
+    @Override
+    public List getValues() {
+        return null;
+    }
+
+    @Override
+    public Set<KeyValue> getKeyValues() {
+        return null;
+    }
+
+    @Override
+    public MyMapInterface invert() {
+        return null;
     }
 }
