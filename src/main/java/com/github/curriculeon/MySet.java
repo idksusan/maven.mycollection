7de0e07613cd42593199ff2c55a418f8c5a0616f
Susan Tabassum <sttabassum93@gmail.com>
2020-01-12 00:41:20 -0500

    Wrote MySet

--- src/main/java/com/github/curriculeon/MySet.java
+++ src/main/java/com/github/curriculeon/MySet.java
@@ -3,41 +3,50 @@ package com.github.curriculeon;
 import java.util.Iterator;
 
 public class MySet<SomeType> implements MyCollectionInterface<SomeType>{
+    private MyCollectionInterface<SomeType> list;
+
     public MySet() {
+        this.list = new MyArrayList<>();
     }
 
     public MySet(SomeType[] valuesToBePopulatedWith) {
-
+        this.list = new MyArrayList<>();
+        for (int i = 0; i < valuesToBePopulatedWith.length; i++){
+            SomeType someType = valuesToBePopulatedWith[i];
+            this.add(someType);
+        }
     }
 
     @Override
     public void add(SomeType objectToAdd) {
-        
+        if (!list.contains(objectToAdd)) {
+            list.add(objectToAdd);
+        }
     }
 
     @Override
     public void remove(SomeType objectToRemove) {
-
+        this.list.remove(objectToRemove);
     }
 
     @Override
     public void remove(int indexOfObjectToRemove) {
-
+        this.list.remove(indexOfObjectToRemove);
     }
 
     @Override
     public SomeType get(int indexOfElement) {
-        return null;
+        return this.list.get(indexOfElement);
     }
 
     @Override
     public Boolean contains(SomeType objectToCheckFor) {
-        return null;
+        return this.list.contains(objectToCheckFor);
     }
 
     @Override
     public Integer size() {
-        return null;
+        return this.list.size();
     }
 
     @Override
